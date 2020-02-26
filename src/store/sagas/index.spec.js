56b783fc78d84884c056e1327f32b131e7da246a
GeorgeTuz <mih_petro@mail.ru>
2020-02-26 18:03:30 +0300

    fix: fix tests

--- src/store/sagas/index.spec.js
+++ src/store/sagas/index.spec.js
@@ -1,15 +1,35 @@
-import { testSaga } from 'redux-saga-test-plan';
+import { testSaga, expectSaga } from 'redux-saga-test-plan';
 // eslint-disable-next-line import/named
 import { init } from './index';
 import BackendServices from '../../BackendServices/backendServices';
 
+jest.mock('../../BackendServices/backendServices', () => ({getMessages: () => {}}));
+
 const getMess = [1, 2, 3];
-it('works with unit tests', () => {
+
+it('1', () => {
+  return expectSaga(init)
+      .provide({
+        call({ fn }, next) {
+          if (fn === BackendServices.getMessages) {
+            return getMess;
+          }
+          return next();
+        },
+      })
+      .put({
+        type: 'ADD_MESSAGES',
+        payload: getMess,
+      })
+      .run();
+});
+
+it('2', () => {
   testSaga(init)
-    .next()
-    .call(BackendServices.getMessages)
-    .next(getMess)
-    .put({ type: 'ADD_MESSAGES', payload: getMess })
-    .next()
-    .isDone();
+      .next()
+      .call(BackendServices.getMessages)
+      .next(getMess)
+      .put({ type: 'ADD_MESSAGES', payload: getMess })
+      .next()
+      .isDone();
 });
